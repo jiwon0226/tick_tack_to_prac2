ef67ed33f34ad3296c159d14d045796007a4fdcd
jiwon0226 <63784453+jiwon0226@users.noreply.github.com>
2024-04-02 05:21:56 +0000

    value값

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
+function Square({ value }) {
+  return <button className="square">{value}</button>; //value</button>; //중복되는 코드 함수화하기
+  //인수값을 받아서 보여주기
+}
+
 export default function board() {
   //함수 정의
   // export는 외부에서 접근할 수 있고, default는 다른 파일에서 주요 함수임을 알려줌
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">1</button>
-        <button className="square">1</button>
+        <Square value="1" />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">3</button>
-        <button className="square">1</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">5</button>
-        <button className="square">x</button>
-      </div>
-      <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">5</button>
-        <button className="square">x</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
